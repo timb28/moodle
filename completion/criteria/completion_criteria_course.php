d89fec48fe3aebddf98c26227f9e407933a34f74
Tim Butler <timb20@gmail.com>
2013-06-25 09:32:06 +1000

    Use course short names in completion report tables

--- completion/criteria/completion_criteria_course.php
+++ completion/criteria/completion_criteria_course.php
@@ -136,8 +136,9 @@ class completion_criteria_course extends completion_criteria {
 
         $prereq = $DB->get_record('course', array('id' => $this->courseinstance));
         $coursecontext = context_course::instance($prereq->id, MUST_EXIST);
-        $fullname = format_string($prereq->fullname, true, array('context' => $coursecontext));
-        return shorten_text(urldecode($fullname));
+        /* Academy Patch: Use course short names in completion report tables */
+        $shortname = format_string($prereq->shortname, true, array('context' => $coursecontext));
+        return shorten_text(urldecode($shortname));
     }
 
     /**
