959d0a52864a36de14902c2c4cc8ecccd73c5ef1
Tim Butler <timb20@gmail.com>
2013-07-09 14:33:45 +1000

    Install plugins script now uses MOODLE_24_STABLE branches for OU question types.

--- install-plugins.sh
+++ install-plugins.sh
@@ -10,8 +10,8 @@ echo /mod/certificate/ >> .git/info/exclude
 # Game
 git clone https://github.com/timb28/moodle-mod_game.git mod/game
 echo /mod/game/ >> .git/info/exclude
-cd mod/game
 git remote add upstream https://github.com/bdaloukas/moodle-mod_game.git
+cd mod/game
 git branch -a
 git branch --track MOODLE_20_STABLE origin/MOODLE_20_STABLE
 git checkout MOODLE_20_STABLE
@@ -20,8 +20,8 @@ cd ../..
 # Questionnaire
 git clone https://github.com/timb28/moodle-mod_questionnaire.git mod/questionnaire
 echo /mod/questionnaire/ >> .git/info/exclude
-cd mod/questionnaire
 git remote add upstream https://github.com/remotelearner/moodle-mod_questionnaire.git
+cd mod/questionnaire
 git branch -a
 git branch --track MOODLE_24_STABLE origin/MOODLE_24_STABLE
 git checkout MOODLE_24_STABLE
@@ -36,6 +36,11 @@ git --git-dir=blocks/configurable_reports/.git remote add upstream https://githu
 git clone https://github.com/timb28/moodle-qtype_ddimageortext.git question/type/ddimageortext
 echo /question/type/ddimageortext/ >> .git/info/exclude
 git --git-dir=question/type/ddimageortext/.git remote add upstream https://github.com/moodleou/moodle-qtype_ddimageortext.git
+cd question/type/ddimageortext
+git branch -a
+git branch --track MOODLE_24_STABLE origin/MOODLE_24_STABLE
+git checkout MOODLE_24_STABLE
+cd ../../..
  
 # OU Drag and Drop Text Quiz Question Type
 git clone https://github.com/timb28/moodle-qtype_ddwtos.git question/type/ddwtos
@@ -46,8 +51,18 @@ git --git-dir=question/type/ddwtos/.git remote add upstream https://github.com/m
 git clone https://github.com/timb28/moodle-qtype_gapselect.git question/type/gapselect
 echo /question/type/gapselect/ >> .git/info/exclude
 git --git-dir=question/type/gapselect/.git remote add upstream https://github.com/moodleou/moodle-qtype_gapselect.git
+cd question/type/gapselect
+git branch -a
+git branch --track MOODLE_24_STABLE origin/MOODLE_24_STABLE
+git checkout MOODLE_24_STABLE
+cd ../../..
  
 # OU Multiple Response Quiz Question Type
 git clone https://github.com/timb28/moodle-qtype_oumultiresponse.git question/type/oumultiresponse
 echo /question/type/oumultiresponse/ >> .git/info/exclude
 git --git-dir=question/type/oumultiresponse/.git remote add upstream https://github.com/moodleou/moodle-qtype_oumultiresponse.git
+cd question/type/oumultiresponse
+git branch -a
+git branch --track MOODLE_24_STABLE origin/MOODLE_24_STABLE
+git checkout MOODLE_24_STABLE
+cd ../../..
