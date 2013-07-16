8e1ecf6fde997faebdb4d6247b8a3675169333a4
Tim Butler <timb20@gmail.com>
2013-07-16 13:25:37 +1000

    Initial commit of Academy Clean Theme.

---
+++ theme/academy_clean/config.php
@@ -0,0 +1,59 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Harcourts Academy Clean theme, based on Moodle's Clean theme.
+ *
+ * For full information about creating Moodle themes, see:
+ * http://docs.moodle.org/dev/Themes_2.0
+ *
+ * @package   theme_academy_clean
+ * @copyright Harcourts International Pty Ltd, 2013 Moodle, moodle.org
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$THEME->name = 'academy_clean';
+
+/////////////////////////////////
+// The only thing you need to change in this file when copying it to
+// create a new theme is the name above. You also need to change the name
+// in version.php and lang/en/theme_academy_clean.php as well.
+//////////////////////////////////
+//
+$THEME->doctype = 'html5';
+$THEME->parents = array('bootstrapbase');
+$THEME->sheets = array('custom');
+$THEME->supportscssoptimisation = false;
+$THEME->yuicssmodules = array();
+
+$THEME->editor_sheets = array();
+
+$THEME->plugins_exclude_sheets = array(
+    'block' => array(
+        'html',
+    ),
+    'gradereport' => array(
+        'grader',
+    ),
+);
+
+$THEME->rendererfactory = 'theme_overridden_renderer_factory';
+$THEME->csspostprocess = 'theme_academy_clean_process_css';
+
+$THEME->blockrtlmanipulations = array(
+    'side-pre' => 'side-post',
+    'side-post' => 'side-pre'
+);
