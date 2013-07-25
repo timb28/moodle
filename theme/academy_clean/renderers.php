089f00b2e0bbbaf52ba476b5da505d95c02b2f42
Tim Butler <timb20@gmail.com>
2013-07-25 16:01:12 +1000

    Academy Clean theme: Added custom rendered to include a link to the main Academy website as the first breadcrumb.

---
+++ theme/academy_clean/renderers.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * A custom renderer for the Academy theme to produce customised content.
+ *
+ * @package    theme
+ * @subpackage academy_clean
+ * @copyright  Harcourts International Pty Ltd
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+class theme_academy_clean_core_renderer extends theme_bootstrapbase_core_renderer {
+
+   /*
+     * This renders the navbar.
+     * Uses bootstrap compatible html.
+     */
+    public function navbar() {
+        $items = $this->page->navbar->get_items();
+        $breadcrumbs = array();
+        $breadcrumbs[] = '<a href="/">Home</a>'; // Make the first breadcrumb a link to our main website.
+        foreach ($items as $item) {
+            $item->hideicon = true;
+            $breadcrumbs[] = $this->render($item);
+        }
+        $divider = '<span class="divider">/</span>';
+        $list_items = '<li>'.join(" $divider</li><li>", $breadcrumbs).'</li>';
+        $title = '<span class="accesshide">'.get_string('pagepath').'</span>';
+        return $title . "<ul class=\"breadcrumb\">$list_items</ul>";
+    } 
+}
+
+?>
