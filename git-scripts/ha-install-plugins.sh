a9f340afbdef0d0e5e3596c1b2fbf53d06024cce
Tim Butler <timb20@gmail.com>
2014-01-08 11:26:20 +1000

    Git script ha-install-plugins.sh updated to point to HarcourtsAcademy github URLs.

--- git-scripts/ha-install-plugins.sh
+++ git-scripts/ha-install-plugins.sh
@@ -13,13 +13,13 @@ echo "Cloning Certificates Module"
  
 ## Configurable Reports
 echo "Cloning Configurable Reports Block"
-(cd .. && git clone https://github.com/timb28/moodle-block_configurablereports.git blocks/configurable_reports)
+(cd .. && git clone https://github.com/HarcourtsAcademy/moodle-block_configurablereports.git blocks/configurable_reports)
 (cd .. && echo /blocks/configurable_reports/ >> .git/info/exclude)
 (cd .. && git --git-dir=blocks/configurable_reports/.git remote add upstream https://github.com/jleyva/moodle-block_configurablereports.git)
  
 ## Game
 echo "Cloning Game Module"
-(cd .. && git clone https://github.com/timb28/moodle-mod_game.git mod/game)
+(cd .. && git clone https://github.com/HarcourtsAcademy/moodle-mod_game.git mod/game)
 (cd .. && echo /mod/game/ >> .git/info/exclude)
 (cd ../mod/game && git remote add upstream https://github.com/bdaloukas/moodle-mod_game.git)
 (cd ../mod/game && git branch -a)
@@ -28,7 +28,7 @@ echo "Cloning Game Module"
  
 ## Questionnaire
 echo "Cloning Questionnaire Module"
-(cd .. && git clone https://github.com/timb28/moodle-mod_questionnaire.git mod/questionnaire)
+(cd .. && git clone https://github.com/HarcourtsAcademy/moodle-mod_questionnaire.git mod/questionnaire)
 (cd .. && echo /mod/questionnaire/ >> .git/info/exclude)
 (cd ../mod/questionnaire && git remote add upstream https://github.com/remotelearner/moodle-mod_questionnaire.git)
 (cd ../mod/questionnaire && git branch -a)
@@ -39,7 +39,7 @@ echo "Cloning Questionnaire Module"
 
 ## Essential
 echo "Cloning Essential Theme"
-(cd .. && git clone https://github.com/timb28/moodle-theme_essential.git theme/essential)
+(cd .. && git clone https://github.com/HarcourtsAcademy/moodle-theme_essential.git theme/essential)
 (cd .. && echo /theme/essential/ >> .git/info/exclude)
 (cd ../theme/essential/ && git remote add upstream https://github.com/moodleman/moodle-theme_essential.git)
 (cd ../theme/essential/ && git branch -a)
@@ -50,24 +50,24 @@ echo "Cloning Essential Theme"
 
 ## OU Drag and Drop Images Quiz Question Type
 echo "Cloning Drag and Drop Image or Text Question Type"
-(cd .. && git clone https://github.com/timb28/moodle-qtype_ddimageortext.git question/type/ddimageortext)
+(cd .. && git clone https://github.com/HarcourtsAcademy/moodle-qtype_ddimageortext.git question/type/ddimageortext)
 (cd .. && echo /question/type/ddimageortext/ >> .git/info/exclude)
 (cd .. && git --git-dir=question/type/ddimageortext/.git remote add upstream https://github.com/moodleou/moodle-qtype_ddimageortext.git)
  
 ## OU Drag and Drop Text Quiz Question Type
 echo "Cloning Drag and Drop Text Question Type"
-(cd .. && git clone https://github.com/timb28/moodle-qtype_ddwtos.git question/type/ddwtos)
+(cd .. && git clone https://github.com/HarcourtsAcademy/moodle-qtype_ddwtos.git question/type/ddwtos)
 (cd .. && echo /question/type/ddwtos/ >> .git/info/exclude)
 (cd .. && git --git-dir=question/type/ddwtos/.git remote add upstream https://github.com/moodleou/moodle-qtype_ddwtos.git)
  
 ## OU Select Missing Words Quiz Question Type
 echo "Cloning Gap Select Question Type"
-(cd .. && git clone https://github.com/timb28/moodle-qtype_gapselect.git question/type/gapselect)
+(cd .. && git clone https://github.com/HarcourtsAcademy/moodle-qtype_gapselect.git question/type/gapselect)
 (cd .. && echo /question/type/gapselect/ >> .git/info/exclude)
 (cd .. && git --git-dir=question/type/gapselect/.git remote add upstream https://github.com/moodleou/moodle-qtype_gapselect.git)
  
 ## OU Multiple Response Quiz Question Type
 echo "Updating Multiple Choice, Multiple Response Question Type"
-(cd .. && git clone https://github.com/timb28/moodle-qtype_oumultiresponse.git question/type/oumultiresponse)
+(cd .. && git clone https://github.com/HarcourtsAcademy/moodle-qtype_oumultiresponse.git question/type/oumultiresponse)
 (cd .. && echo /question/type/oumultiresponse/ >> .git/info/exclude)
 (cd .. && git --git-dir=question/type/oumultiresponse/.git remote add upstream https://github.com/moodleou/moodle-qtype_oumultiresponse.git )
