c0ff130254ca5648a074079e449d8bc5dd77e22b
Tim Butler <timb20@gmail.com>
2014-03-26 14:02:37 +1000

    Merged HA-2014-03-HarcourtsOneEnrolment into MOODLE_25_STABLE.

--- enrol/harcourtsone/version.php
+++ enrol/harcourtsone/version.php
@@ -24,6 +24,6 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2014010106;        // The current plugin version (Date: YYYYMMDDXX)
+$plugin->version   = 2014032601;        // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2013050100;        // Requires this Moodle version
 $plugin->component = 'enrol_harcourtsone';      // Full name of the plugin (used for diagnostics)
