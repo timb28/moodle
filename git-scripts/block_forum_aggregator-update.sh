7caf57ee3f65165ed62915c08b8b23b6dd5f8d16
Tim Butler <timb20@gmail.com>
2014-05-07 15:06:26 +1000

    Git script block_forum_aggregator updated to use the latest Harcourts Academy branch.

--- git-scripts/block_forum_aggregator-update.sh
+++ git-scripts/block_forum_aggregator-update.sh
@@ -1,2 +1,2 @@
 echo "Updating Forum Aggregator Block"
-(cd ../blocks/forum_aggregator/ && git fetch upstream && git pull upstream 2013091300)
+(cd ../blocks/forum_aggregator/ && git fetch upstream && git pull upstream HA-BlockForumAggregator)
