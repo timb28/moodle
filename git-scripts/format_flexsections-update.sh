b604c94c4f75a24a9a2c42cb3e9201c450db7c90
Tim Butler <timb20@gmail.com>
2014-05-08 16:17:20 +1000

    Updated course flex sections git scripts to use HA-CourseFlexSections branch.

--- git-scripts/format_flexsections-update.sh
+++ git-scripts/format_flexsections-update.sh
@@ -1,3 +1,2 @@
-## Flexible sections format
 echo "Updating Flexible sections course format"
-(cd ../course/format/flexsections/ && git fetch upstream && git pull upstream master)
+(cd ../course/format/flexsections/ && git fetch upstream && git pull upstream HA-CourseFlexSections)
