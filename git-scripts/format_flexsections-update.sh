2307a002818c94d77f4bc2bb9aa9cfd1977f6965
Tim Butler <timb20@gmail.com>
2014-05-20 16:32:42 +1000

    Git script for updating format_flexsections now uses origin rather than upstream.

--- git-scripts/format_flexsections-update.sh
+++ git-scripts/format_flexsections-update.sh
@@ -1,2 +1,2 @@
 echo "Updating Flexible sections course format"
-(cd ../course/format/flexsections/ && git fetch upstream && git pull upstream HA-CourseFlexSections)
+(cd ../course/format/flexsections/ && git fetch origin && git pull origin HA-CourseFlexSections)
