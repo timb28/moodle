6fe8272da0861e8bf7498b923544b97f004bcc9e
Tim Butler <timb20@gmail.com>
2014-09-24 09:03:09 +1000

    Removed unused mod_checklist git scripts.

--- git-scripts/mod_checklist-install.sh
+++
@@ -1,8 +0,0 @@
-## Checklist
-echo "Cloning Certificates Module"
-(cd .. && git clone https://github.com/davosmith/moodle-checklist.git mod/checklist)
-(cd .. && git --git-dir=mod/checklist/.git remote add upstream https://github.com/davosmith/moodle-checklist.git)
-(cd .. && echo /mod/checklist/ >> .git/info/exclude)
-## (cd ../mod/checklist && git branch -a)
-## (cd ../mod/checklist && git branch --track HA-Moodle25 origin/HA-Moodle25)
-## (cd ../mod/checklist && git checkout HA-Moodle25) 
