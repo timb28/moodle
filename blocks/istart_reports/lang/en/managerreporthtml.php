cf7d6ea4d8d950d2faf5b69d3592d29a1b621db0
Tim Butler <timb20@gmail.com>
2014-12-01 05:34:53 +1000

    blocks_istart_reports: dashboard icons in the manger report no longer scaled.

--- blocks/istart_reports/lang/en/managerreporthtml.php
+++ blocks/istart_reports/lang/en/managerreporthtml.php
@@ -1,4 +1,5 @@
 <?php
+require_once('../../../../config.php');
 $string['managerreporthtml'] = '
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd"><html xmlns="http://www.w3.org/1999/xhtml"><head><!-- NAME: 2 COLUMN -->
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
@@ -765,4 +766,8 @@ $string['managerreporthtml'] = '
     </body>
 </html>
         ';
+
+//TODO: REmove the following:
+echo($string['managerreporthtml']);
+opcache_reset(); // TODO: Remove this line
 ?>
