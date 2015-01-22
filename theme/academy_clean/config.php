c3f8afb8e9a4a888d934b1edcb33068bac39f547
Tim Butler <timb20@gmail.com>
2015-01-22 16:22:30 +1000

    Added custom css for mod_feedback used for AVETMISS 7 enrolment questionnaire.

--- theme/academy_clean/config.php
+++ theme/academy_clean/config.php
@@ -36,7 +36,7 @@ $THEME->name = 'academy_clean';
 $THEME->doctype = 'html5';
 $THEME->parents = array('bootstrapbase');
 $THEME->sheets = array('academy','course','content','form','questionnaire',
-    'joomla','print','custom','videofile','istart');
+    'joomla','print','custom','videofile','istart','mod_feedback');
 $THEME->supportscssoptimisation = false;
 $THEME->yuicssmodules = array();
 
