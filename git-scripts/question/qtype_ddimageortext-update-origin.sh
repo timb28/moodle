22a7d1fe403247ee4c843e56ad4165d7ff8a934a
Tim Butler <timb20@gmail.com>
2015-02-24 11:34:44 +1000

    Git script for updating qtype_ddimageortext from origin now uses the HA-master branch that contains fixes.

--- git-scripts/question/qtype_ddimageortext-update-origin.sh
+++ git-scripts/question/qtype_ddimageortext-update-origin.sh
@@ -4,4 +4,4 @@
 ##
 ## OU Drag and Drop Images Quiz Question Type
 echo "Updating Drag and Drop Image or Text Question Type from Origin"
-(cd ../question/type/ddimageortext && git fetch origin && git pull origin master)
+(cd ../question/type/ddimageortext && git fetch origin && git pull origin HA-master)
