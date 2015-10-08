3dba1e1ca1240a6b963e9a85870b203b43538cde
Tim Butler <tim.butler@harcourts.net>
2015-10-08 20:29:05 +1000

    git script for block_progress now uses the Harcourts Academy origin url.

--- git-scripts/blocks/block_progress-install.sh
+++ git-scripts/blocks/block_progress-install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 ## Used by:
-## ---=== TESTING ===---
+## - iStart Online v2
 ##
-echo "Cloning Progress Bar Block from Upstream"
-(cd .. && git clone https://github.com/deraadt/Moodle-block_progress.git blocks/progress)
+echo "Cloning Progress Bar Block from Origin"
+(cd .. && git clone https://github.com/HarcourtsAcademy/Moodle-block_progress.git blocks/progress)
 (cd .. && echo /blocks/progress/ >> .git/info/exclude)
 (cd .. && git --git-dir=blocks/progress/.git remote add upstream https://github.com/deraadt/Moodle-block_progress.git)
