4f807f64219798791a22c71833d6f77629c4e074
Tim Butler <tim.butler@harcourts.net>
2015-10-09 08:58:49 +1000

    mod_subcourse git script now uses the Harcourts Academy github fork as origin.

--- git-scripts/mod/mod_subcourse-install.sh
+++ git-scripts/mod/mod_subcourse-install.sh
@@ -1,5 +1,5 @@
 ## Sub-course
-echo "Cloning Sub-course Module"
-(cd .. && git clone https://github.com/mudrd8mz/moodle-mod_subcourse.git mod/subcourse)
+echo "Cloning Sub-course Module from origin"
+(cd .. && git clone https://github.com/HarcourtsAcademy/moodle-mod_subcourse.git mod/subcourse)
 (cd .. && git --git-dir=mod/subcourse/.git remote add upstream https://github.com/mudrd8mz/moodle-mod_subcourse.git)
 (cd .. && echo /mod/subcourse/ >> .git/info/exclude)
