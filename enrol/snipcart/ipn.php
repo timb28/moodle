a89ca7f889ad906500cf342e8bb23b7afa32c08e
Tim Butler <tim.butler@harcourts.net>
2015-10-28 15:30:01 +1000

    enrol_snipcart: Added ipn webhook script skeleton.

---
+++ enrol/snipcart/ipn.php
@@ -0,0 +1,50 @@
+<?php
+
+/**
+ * Listens for payment notification from Snipcart
+ *
+ * This script waits for payment notification from Snipcart,
+ * then double checks that data by sending it back to Snipcart.
+ * If Snipcart verifies this then it sets up the enrolment for that
+ * user.
+ *
+ * @package   enrol_snipcart
+ * @author    Tim Butler
+ * @copyright (c) 2015 Harcourts International Limited {@link http://www.harcourtsacademy.com}
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+// Disable moodle specific debug messages and any errors in output,
+// comment out when debugging or better look into error log!
+define('NO_DEBUG_DISPLAY', true);
+
+require("../../config.php");
+require_once("lib.php");
+
+$json = file_get_contents('php://input');
+
+/// Keep out casual intruders
+if (empty($json) or !empty($_GET)) {
+    header('HTTP/1.1 400 Bad Request');
+    return;
+}
+
+$body = json_decode($json, true);
+
+if (is_null($body) or !isset($body['eventName'])) {
+    // When something goes wrong, return an invalid status code
+    // such as 400 BadRequest.
+    header('HTTP/1.1 400 Bad Request');
+    return;
+}
+
+switch ($body['eventName']) {
+    case 'order.completed':
+        // This is an order:completed event
+        // do what needs to be done here.
+        error_log('Snipcart: order complete');
+        break;
+}
+
+// Return a valid status code such as 200 OK.
+header('HTTP/1.1 200 OK');
