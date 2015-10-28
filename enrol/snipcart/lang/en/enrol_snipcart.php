8525b1aa87a7a090d11986532454525fe91a0a7a
Tim Butler <tim.butler@harcourts.net>
2015-10-28 16:15:54 +1000

    enrol_snipcart: Added access language strings.

--- enrol/snipcart/lang/en/enrol_snipcart.php
+++ enrol/snipcart/lang/en/enrol_snipcart.php
@@ -11,3 +11,7 @@
 
 $string['pluginname'] = 'Snipcart';
 $string['pluginname_desc'] = 'The Snipcart module allows you to set up paid courses.  If the cost for any course is zero, then students are not asked to pay for entry.';
+$string['snipcart:config'] = 'Configure Snipcart enrol instances';
+$string['snipcart:manage'] = 'Manage enrolled users';
+$string['snipcart:unenrol'] = 'Unenrol users from course';
+$string['snipcart:unenrolself'] = 'Unenrol self from the course';
