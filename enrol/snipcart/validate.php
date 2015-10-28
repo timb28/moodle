f5964402f4addbe1babdec603f9a03b6f425b41e
Tim Butler <tim.butler@harcourts.net>
2015-10-28 15:54:20 +1000

    enrol_snipcart: Added initial validation script for Snipcart to call to validate prices and public APIs.

---
+++ enrol/snipcart/validate.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+
+        <script type="text/javascript" src="https://cdn.snipcart.com/scripts/snipcart.js" id="snipcart"
+          data-api-key="<?php echo('test'); ?>"></script>
+
+        <link type="text/css" href="https://cdn.snipcart.com/themes/base/snipcart.min.css" rel="stylesheet" />
+    </body>
+</html>
