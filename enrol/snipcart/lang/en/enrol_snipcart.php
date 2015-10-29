241469684f36358eb644cf0846f728678f805aa6
Tim Butler <tim.butler@harcourts.net>
2015-10-29 11:15:35 +1000

    enrol_snipcart: Now includes site and enrolment instance default settings.

--- enrol/snipcart/lang/en/enrol_snipcart.php
+++ enrol/snipcart/lang/en/enrol_snipcart.php
@@ -11,16 +11,24 @@
 
 $string['assignrole'] = 'Assign role';
 $string['crontask'] = 'Snipcart enrolment scheduled tasks';
+$string['defaultrole'] = 'Default role assignment';
+$string['defaultrole_desc'] = 'Select role which should be assigned to users during Snipcart enrolments';
 $string['enrolenddate'] = 'End date';
 $string['enrolenddate_help'] = 'If enabled, users can be enrolled until this date only.';
 $string['enrolperiod'] = 'Enrolment duration';
+$string['enrolperiod_desc'] = 'Default length of time that the enrolment is valid. If set to zero, the enrolment duration will be unlimited by default.';
 $string['enrolperiod_help'] = 'Length of time that the enrolment is valid, starting with the moment the user is enrolled. If disabled, the enrolment duration will be unlimited.';
 $string['enrolstartdate'] = 'Start date';
 $string['enrolstartdate_help'] = 'If enabled, users can be enrolled from this date onward only.';
+$string['expiredaction'] = 'Enrolment expiration action';
+$string['expiredaction_help'] = 'Select action to carry out when user enrolment expires. Please note that some user data and settings are purged from course during course unenrolment.';
 $string['pluginname'] = 'Snipcart';
 $string['pluginname_desc'] = 'The Snipcart module allows you to set up paid courses.  If the cost for any course is zero, then students are not asked to pay for entry.';
+$string['publicapikey'] = 'Snipcart Public API Key';
+$string['publicapikey_desc'] = 'The public API key is the key you need to add on your website. This key can be shared without security issues because it only allows a specific subset of API operations.';
 $string['snipcart:config'] = 'Configure Snipcart enrol instances';
 $string['snipcart:manage'] = 'Manage enrolled users';
 $string['snipcart:unenrol'] = 'Unenrol users from course';
 $string['snipcart:unenrolself'] = 'Unenrol self from the course';
 $string['status'] = 'Allow Snipcart enrolments';
+$string['status_desc'] = 'Allow users to use Snipcart to enrol into a course by default.';
