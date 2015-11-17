01a14b780c4fc46282d29c5549c7029bba442c36
Tim Butler <tim.butler@harcourts.net>
2015-11-17 11:49:38 +1000

    block_snipcart: Shopping cart now appears with basic cart image and styling.

--- blocks/snipcart/block_snipcart.php
+++ blocks/snipcart/block_snipcart.php
@@ -41,11 +41,13 @@ class block_snipcart extends block_base {
     }
 
     public function get_content() {
-        global $USER;
+        global $OUTPUT, $PAGE, $USER;
         
         if ($this->content !== null) {
             return $this->content;
         }
+        
+        $PAGE->requires->css('/blocks/snipcart/styles.css');
         
         $plugin = enrol_get_plugin('snipcart');
         
@@ -60,11 +62,12 @@ class block_snipcart extends block_base {
         $shoppingcart.= '   data-api-key="'. $publicapikey .'"></script>';
         $shoppingcart.= '<link type="text/css" href="https://cdn.snipcart.com/themes/base/snipcart.min.css" rel="stylesheet" />';
         $shoppingcart.= '<div class="snipcart-summary">';
-        $shoppingcart.= '  <div>Number of items: <span class="snipcart-total-items"></span></div>';
-        $shoppingcart.= '  <div>Total price: <span class="snipcart-total-price"></span></div>';
+        $shoppingcart.= '  <div class="snipcart-image"><a href="#" class="snipcart-checkout"><img src="'.$OUTPUT->pix_url('shopping_cart', 'block_snipcart').'" /></a></div>';
+        $shoppingcart.= '  <div class="snipcart-totals"><span class="snipcart-total-items-label">Items:</span> <span class="snipcart-total-items"></span>';
+        $shoppingcart.= '  <span class="snipcart-total-price-label">Total: </span><span class="snipcart-total-price"></span></div>';
         $shoppingcart.= '</div>';
         $shoppingcart.= '<div class="checkout">';
-        $shoppingcart.= '  <a href="#" class="snipcart-checkout btn btn-success">' . get_string('checkout', 'block_snipcart') . '</a>';
+        $shoppingcart.= '  <a href="#" class="snipcart-checkout btn btn-small btn-success">' . get_string('checkout', 'block_snipcart') . '</a>';
         $shoppingcart.= '</div>';
         
         $this->content         =  new stdClass;
