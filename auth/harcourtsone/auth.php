169aaa64b1c48ce2f2303c0115e55e29fc0cf72b
Tim Butler <tim.butler@harcourts.net>
2016-01-14 13:56:26 +1000

    Re-added auth_harcourtsone (with new version.php). It's needed to authenticate students access our online sales workshops.

---
+++ auth/harcourtsone/auth.php
@@ -0,0 +1,27 @@
+<?php
+/* 
+ * Empty class as all the functionality is in index.php
+ */
+
+if (!defined('MOODLE_INTERNAL')) {
+    die('Direct access to this script is forbidden.');    ///  It must be included from a Moodle page
+}
+
+require_once($CFG->libdir.'/authlib.php');
+
+class auth_plugin_harcourtsone extends auth_plugin_base {
+    /**
+     * Constructor.
+     */
+    function auth_plugin_harcourtsone() {
+        $this->authtype = 'harcourtsone';
+    }
+    
+    /**
+     * Do not allow any direct login.
+     *
+     */
+    function user_login($username, $password) {
+        return false;
+    }
+}
