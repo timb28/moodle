19ba2f9a2c11e841f6c88ebc73f3828dc949ea11
Tim Butler <tim.butler@harcourts.net>
2016-03-18 09:46:15 +1000

    theme_academy_clean: theme_academy_clean_block_course_overview_renderer::course_overview() now doesn't display the mnethostname at all.

--- theme/academy_clean/renderers.php
+++ theme/academy_clean/renderers.php
@@ -498,7 +498,6 @@ class theme_academy_clean_block_course_overview_renderer extends block_course_ov
                 $html .= $this->output->heading(html_writer::link(
                     $course->wwwroot . '/course/view.php?id=' . $course->remoteid,
                     format_string($course->shortname, true), $attributes), 2, 'title');
-                $html .= $this->output->container(format_string($course->hostname), 'mnethostname');
                 $html .= theme_academy_create_course_button($course);
             } else {
                 $html .= $this->output->heading(html_writer::link(
