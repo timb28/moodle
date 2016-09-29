e6a4147dc64cdba9df7d40cd23a6cb50a3ad3352
Tim Butler <tim.butler@harcourts.net>
2016-09-30 09:16:45 +1000

    Updated mod_hvp git-scripts to use 1.0-rc.3 release.

--- git-scripts/mod/mod_h5p-install.sh
+++ git-scripts/mod/mod_h5p-install.sh
@@ -1,6 +1,6 @@
-## Game
+## H5P
 echo "Cloning H5P Module"
 (cd .. && git clone https://github.com/h5p/h5p-moodle-plugin.git mod/hvp)
 (cd .. && cd mod/hvp && git submodule update --init)
 (cd .. && echo /mod/hvp/ >> .git/info/exclude)
-(cd ../mod/hvp && git checkout 1.0-rc.2)
+(cd ../mod/hvp && git checkout 1.0-rc.3)
--- git-scripts/mod/mod_h5p-update-origin.sh
+++ git-scripts/mod/mod_h5p-update-origin.sh
@@ -1,4 +1,4 @@
-## Game
+## H5P
 echo "Updating H5P Module"
-(cd ../mod/hvp && git fetch origin && git checkout master)
+(cd ../mod/hvp && git fetch origin && git checkout 1.0-rc.3)
 (cd ../mod/hvp && git submodule update --recursive)
