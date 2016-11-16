de3b04d909f1afd264995de4efc9afe3a286f0c1
Tim Butler <tim.butler@harcourts.net>
2016-11-16 13:50:52 +1000

    Removed unused testcode in istart reports block.

--- blocks/istart_reports/testblock8742.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// TODO remove entire file after testing
-
-require_once("../../config.php");
-require_once("$CFG->dirroot/blocks/istart_reports/lib.php");
-
-opcache_reset();
-istart_reports_cron();
