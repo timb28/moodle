a7984cf6d337546fe76a2505bfbb7f47cc6f941b
Tim Butler <tim.butler@harcourts.net>
2016-11-21 21:59:30 +1000

    filter_academylang: Added word segments replacements to handle common US dictionary changes such as anal(yse), col(our) and capita(lise).

--- filter/academylang/filter.php
+++ filter/academylang/filter.php
@@ -30,76 +30,52 @@ class filter_academylang extends moodle_text_filter {
      */
     private $dictionaries = array(
         'US' => array(
-            'analyse' => 'analyze', 'analyses' => 'analyzes', 'analysed' => 'analyzed',
             'appraisal' => 'comparative market analysis', 'appraisals' => 'comparative market analyses',
             'auction clearance rate' => 'auction success rate',
-            'behaviour' => 'behavior', 'behavioural' => 'behavioral',
             'benchtop' => 'counter top', 'benchtops' => 'counter tops',
-            'bluebook' => 'blue&#8203;book (not used in USA)',
+            'bluebook' => 'blue&#8203;book (Not Used in the USA)',
             'body corporate' => 'homeowners association',
             'Campaign Track' => 'Create One',
-            'capitalise' => 'capitalize', 'capitalises' => 'capitalize', 'capitalised' => 'capitalized',
             'car boot' => 'trunk',
             'categorise' => 'categorize', 'categorised' => 'categorized', 'categorising' => 'categorizing',
             'centre' => 'center', 'centred' => 'centered',
             'chattels' => 'included items',
             'cheque' => 'check', 'cheques' => 'checks',
-            'colour' => 'color', 'colours' => 'colors',
             'computerise' => 'computerize', 'computerised' => 'computerized',
             'consultant' => 'agent', 'consultants' => 'agents',
             'conveyancing' => 'escrow',
             'defence' => 'defense',
-            'demeanour' => 'demeanor',
             'diarise' => 'calendar',
             'doorknock' => 'going door to door', 'doorknocking' => 'going door to door',
             'emphasise' => 'emphasize', 'emphasises' => 'emphasizes', 'emphasised' => 'emphasized',
-            'endeavour' => 'endeavor', 'endeavours' => 'endeavors', 'endeavouring' => 'endeavoring',
             'enquiry' => 'inquiry',
             'familiarise' => 'familiarize', 'familiarises' => 'familiarizes', 'familiarised' => 'familiarized',
-            'favour' => 'favor', 'favours' => 'favors',
-            'favourable' => 'favorable',
-            'favourite' => 'favorite', 'favourites' => 'favorites',
             'fixtures' => ' fixtures',
             'fortnightly' => 'bi-weekly',
             'fulfil' => 'fulfill', 'fulfils' => 'fulfills',
             'garage sale' => 'yard sale', 'garage sales' => 'yard sales',
-            'harmonise' => 'harmonize', 'harmonises' => 'harmonizes', 'harmonised' => 'harmonized',
             'high school' => 'secondary school', 'high schools' => 'secondary schools',
-            'honour' => 'honor', 'honours' => 'honors', 'honoured' => 'honored',
             'house' => 'single family residence',
-            'humanise' => 'humanize', 'humanises' => 'humanizes', 'humanised' => 'humanized',
-            'humour' => 'humor',
             'inspection' => 'showing', 'inspections' => 'showings',
             'inspect' => 'view',
             'kindergarten' => 'preschool',
-            'jeopardise' => 'jeopardize', 'jeopardises' => 'jeopardizes', 'jeopardised' => 'jeopardized',
             'primary school' => 'elementary school', 'primary schools' => 'elementary schools',
             'land size' => 'lot size',
             'letter box dropping' => 'going door to door', 'letterbox drop' => 'door drop',
             'lucky dip prize' => 'grab bag', 'lucky dip prizes' => 'grab bags',
             'listing authority' => 'listing agreement',
             'memorise' => 'memorize', 'memorises' => 'memorizes', 'memorised' => 'memorized',
-            'maximise' => 'maximize', 'maximises' => 'maximizes', 'maximised' => 'maximized',
             'mobile phone' => 'cell phone', 'mobile phones' => 'cell phones',
-            'neighbour' => 'neighbor', 'neighbours' => 'neighbors',
-            'neighbourhood' => 'neighborhood', 'neighbourhoods' => 'neighborhoods',
             'open home' => 'open house', 'open homes' => 'open houses',
             'organisation' => 'organization', 'organisations' => 'organizations',
             'organisational' => 'organizational',
-            'organise' => 'organize', 'organises' => 'organizes', 'organised' => 'organized',
-            'organiser' => 'organizer', 'organising' => 'organizing',
             'pay rise' => 'pay raise',
-            'personalise' => 'personalize', 'personalised' => 'personalized',
             'petrol' => 'gas',
             'practise' => 'practice', 'practises' => 'practices', 'practised' => 'practiced',
-            'prioritise' => 'prioritize', 'prioritises' => 'prioritizes', 'prioritised' => 'prioritized',
             'private seller' => 'for sale by owner (FSBO)', 'private sellers' => 'for sale by owners (FSBOs)',
             'property inspection' => 'showing', 'property inspections' => 'showings',
             'programme' => 'program', 'programmes' => 'programs',
             'rates' => 'property taxes',
-            'realise' => 'realize', 'realises' => 'realizes', 'realised' => 'realized',
-            'recognise' => 'recognize', 'recognises' => 'recognizes', 'recognised' => 'recognized',
-            'revitalise' => 'revitalize', 'revitalises' => 'revitalizes', 'revitalised' => 'revitalized',
             'RPData' => 'Core Logic',
             'rubbish' => 'garbage',
             'Sale &amp; Purchase Agreement' => 'Contract of Sale',
@@ -111,21 +87,32 @@ class filter_academylang extends moodle_text_filter {
             'settled' => 'closed',
             'sqm' => 'sq ft',
             'solicitor' => 'attorney', 'solicitors' => 'attorneys',
-            'specialise' => 'specialize', 'specialises' => 'specializes', 'specialised' => 'specialized',
-            'summarise' => 'summarize', 'summarises' => 'summarizes', 'summarised' => 'summarized',
             'superannuation' => 'social security pension',
             'thermal control' => 'temperature control',
             'tick off the list' => 'check off the list',
             'for rent' => 'to let',
             'unit' => 'condo', 'units' => 'condos',
-            'utilise' => 'utilize', 'utilises' => 'utilizes', 'utilised' => 'utilized',
             'valuer' => 'appraiser',
             'valuation' => 'appraisal',
             'vendor' => 'seller', 'vendors' => 'sellers',
             'VPA' => 'SPA',
             'vendor paid advertising' => 'seller paid advertising',
-            'vocalise' => 'vocalize', 'vocalises' => 'vocalizes', 'vocalised' => 'vocalized',
             'whilst' => 'while',
+        )
+    );
+
+    /** @var string[] Country specific word segments for localisation.
+     * Use zero-width space (&#8203;) to prevent recursive replacements.
+     */
+    private $segments = array(
+        'US' => array(
+            '(\w+)yse' => '${1}yze', '(\w+)yses' => '${1}yzes', '(\w+)ysed' => '${1}yzed',
+            '(\w+[dlmntv])ise' => '${1}ize',
+            '(\w+[dlmntv])ised' => '${1}ized',
+            '(\w+[dlmntv])ises' => '${1}izes',
+            '(\w+[dlmntv])iser' => '${1}izer',
+            '(\w+[dlmntv])ising' => '${1}izing',
+            '(\w+)our(\w*)' => '${1}or${2}',
         )
     );
 
@@ -150,7 +137,6 @@ class filter_academylang extends moodle_text_filter {
                 }
             }
         }
-        // error_log('dict: ' . print_r($this->dictionaries, true));
     }
 
     /**
@@ -168,18 +154,18 @@ class filter_academylang extends moodle_text_filter {
             return $text;
         }
 
-        foreach ($this->dictionaries as $country => $dictionary) {
-            if ($USER->country != $country) {
-                return $text;
-            }
-
-            foreach ($dictionary as $search => $replace) {
-                $text = preg_replace_callback("/\b(" . $search . ")\b/",
-                    function($match) use ($replace) {
-                        return($replace);
-                    },  $text);
-            }
+        if (!isset($USER->country)) {
+            return $text;
         }
+
+        foreach ($this->dictionaries[$USER->country] as $search => $replace) {
+            $text = preg_replace("/\b(" . $search . ")\b/", $replace,  $text);
+        }
+
+        foreach ($this->segments[$USER->country] as $search => $replace) {
+            $text = preg_replace("/\b" . $search . "\b/", $replace,  $text);
+        }
+
         return $text;
     }
 }
