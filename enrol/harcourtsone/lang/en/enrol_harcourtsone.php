67d352f18389a210427f6baec4ba6314e5740db5
Tim Butler <tim.butler@harcourts.net>
2017-04-05 11:59:47 +1000

    'Harcourts One' corrected to 'HarcourtsOne' in all enrol_harcourtsone language strings.

--- enrol/harcourtsone/lang/en/enrol_harcourtsone.php
+++ enrol/harcourtsone/lang/en/enrol_harcourtsone.php
@@ -22,16 +22,16 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-$string['enrolinstructions'] = '<strong>You are not enrolled in this course.</strong></br>To enrol, please register using Harcourts One using the following options.';
+$string['enrolinstructions'] = '<strong>You are not enrolled in this course.</strong></br>To enrol, please register using HarcourtsOne using the following options.';
 $string['enrolbutton'] = '<i class="icon-user"></i> Register';
 $string['reloadinstructions'] = '<strong>Note:</strong> Your enrollment can take up to 10 minutes to activate.';
 $string['reloadbutton'] = '<i class="icon-refresh"></i> Retry';
-$string['pluginname'] = 'Harcourts One';
-$string['pluginname_desc'] = 'The Harcourts One enrolment plugin allows you to direct potential students to a Harcourts One course to register.';
-$string['customtext1'] = 'Harcourts One URL';
+$string['pluginname'] = 'HarcourtsOne';
+$string['pluginname_desc'] = 'The Harcourts One enrolment plugin allows you to direct potential students to a HarcourtsOne course to register.';
+$string['customtext1'] = 'HarcourtsOne URL';
 $string['logininstructions'] = 'Please login to continue';
 $string['loginbutton'] = 'Login';
-$string['harcourtsone:config'] = 'Configure Harcourts One enrol instances';
-$string['harcourtsone:manage'] = 'Manage Harcourts One user enrolments';
-$string['status'] = 'Allow Harcourts One enrolments';
-$string['nourl'] = 'The website administrator needs to configure a Harcourts One URL or select another enrolment method.';
+$string['harcourtsone:config'] = 'Configure HarcourtsOne enrol instances';
+$string['harcourtsone:manage'] = 'Manage HarcourtsOne user enrolments';
+$string['status'] = 'Allow HarcourtsOne enrolments';
+$string['nourl'] = 'The website administrator needs to configure a HarcourtsOne URL or select another enrolment method.';
