43d670ac1d040119e63ac7fb5e7819016e39a28e
Tim Butler <tim.butler@harcourts.net>
2017-04-28 13:53:29 +1000

    block_training_pathways now requires Moodle 3.0.9+

--- blocks/training_pathways/version.php
+++ blocks/training_pathways/version.php
@@ -26,7 +26,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version   = 2017031402;        // The current plugin version (Date: YYYYMMDDXX)
-$plugin->requires  = 2012112900;        // Requires this Moodle version
+$plugin->requires  = 2015111609;        // Requires this Moodle version
 $plugin->component = 'block_training_pathways'; // Full name of the plugin (used for diagnostics)
 $plugin->maturity = MATURITY_ALPHA;
 $plugin->release = 'v0.1';
