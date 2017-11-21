6ceab641c03caa9c67c8e7bc734a5048e7052c38
Tim Butler <tim.butler@harcourts.net>
2017-11-21 12:01:35 +1000

    Added theme_academyboost as a uncustomised child of the Boost theme.

---
+++ theme/academyboost/lang/en/theme_academyboost.php
@@ -0,0 +1,80 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'theme_academy_clean', language 'en'
+ *
+ * @package   theme_academyboost
+ * @copyright 2017 Harcourts International Ltd
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+// Course category buttons
+//$string['inprogress'] = '<a href="{$a}" class="btn btn-small btn-info pull-right"><i class="icon-chevron-right icon-white"></i></a>';
+//$string['complete'] = '<a href="{$a}" class="btn btn-success btn-small pull-right"><i class="icon-ok icon-white"></i></a>';
+
+$string['choosereadme'] = 'Academy Boost is a modified Moodle boost theme.';
+$string['configtitle'] = 'Academy Boost';
+
+// The name of the second tab in the theme settings.
+$string['advancedsettings'] = 'Advanced settings';
+// The brand colour setting.
+$string['brandcolor'] = 'Brand colour';
+// The brand colour setting description.
+$string['brandcolor_desc'] = 'The accent colour.';
+// Name of the settings pages.
+$string['configtitle'] = 'Academy Boost settings';
+// Name of the first settings tab.
+$string['generalsettings'] = 'General settings';
+// Preset files setting.
+$string['presetfiles'] = 'Additional theme preset files';
+// Preset files help text.
+$string['presetfiles_desc'] = 'Preset files can be used to dramatically alter the appearance of the theme. See <a href=https://docs.moodle.org/dev/Boost_Presets>Boost presets</a> for information on creating and sharing your own preset files, and see the <a href=http://moodle.net/boost>Presets repository</a> for presets that others have shared.';
+// Preset setting.
+$string['preset'] = 'Theme preset';
+// Preset help text.
+$string['preset_desc'] = 'Pick a preset to broadly change the look of the theme.';
+// Raw SCSS setting.
+$string['rawscss'] = 'Raw SCSS';
+// Raw SCSS setting help text.
+$string['rawscss_desc'] = 'Use this field to provide SCSS or CSS code which will be injected at the end of the style sheet.';
+// Raw initial SCSS setting.
+$string['rawscsspre'] = 'Raw initial SCSS';
+// Raw initial SCSS setting help text.
+$string['rawscsspre_desc'] = 'In this field you can provide initialising SCSS code, it will be injected before everything else. Most of the time you will use this setting to define variables.';
+
+//$string['piwiksiteid'] = 'Piwik Site ID';
+//$string['piwiksiteiddesc'] = 'For tracking web site visitors. Enter zero to disable tracking.';
+
+//$string['customcss'] = 'Custom CSS';
+//$string['customcssdesc'] = 'Whatever CSS rules you add to this textarea will be reflected in every page, making for easier customization of this theme.';
+//
+//$string['footnote'] = 'Footnote';
+//$string['footnotedesc'] = 'Whatever you add to this textarea will be displayed in the footer throughout your Moodle site.';
+//
+//$string['invert'] = 'Invert navbar';
+//$string['invertdesc'] = 'Swaps text and background for the navbar at the top of the page between black and white.';
+//
+//$string['next'] = 'Next: ';
+
+$string['pluginname'] = 'Academy Boost';
+
+$string['region-side-post'] = 'Right';
+$string['region-side-pre'] = 'Left';
+$string['region-center'] = 'Center';
+
