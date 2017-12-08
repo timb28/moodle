bfc4ddb590545e3c5f6a68122997206211e1d1c2
Tim Butler <tim.butler@harcourts.net>
2017-12-08 14:39:43 +1000

    block_ha_myoverview: Re-added db/access.php and bumped version number.

--- blocks/ha_myoverview/version.php
+++ blocks/ha_myoverview/version.php
@@ -24,6 +24,6 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2017120800;         // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2017120801;         // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2017050500;         // Requires this Moodle version.
 $plugin->component = 'block_ha_myoverview'; // Full name of the plugin (used for diagnostics).
