3d153ed5c8c3931a830a7e5e27cbe97b5fa8ed03
Tim Butler <tim.butler@harcourts.net>
2019-07-03 13:46:12 +1000

    Force password change on first login when user created via webservice moodle/user:create

--- user/externallib.php
+++ user/externallib.php
@@ -244,6 +244,8 @@ class core_user_external extends external_api {
                 setnew_password_and_mail($userobject);
                 unset_user_preference('create_password', $userobject);
                 set_user_preference('auth_forcepasswordchange', 1, $userobject);
+            } else {
+                set_user_preference('auth_forcepasswordchange', 1, $userobject);
             }
 
             // Trigger event.
