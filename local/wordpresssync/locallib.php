e02555feebf88a35473bfe507282bf07aaa78bca
Tim Butler <tim.butler@harcourts.net>
2020-06-26 14:30:10 +1000

    Initial commit of local_wordpressync

---
+++ local/wordpresssync/locallib.php
@@ -0,0 +1,35 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Plugin internal classes, functions and constants are defined here.
+ *
+ * @package     local_wordpresssync
+ * @copyright   2020 Harcourts International Pty Ltd <academy@harcourts.net>
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+/**
+ * Handle the \core\event\user_created event.
+ *
+ * @param object $event The event object.
+ */
+function user_created($event) {
+    error_log("Moodle User Created:" . print_r($event, true));
+    return;
+}
